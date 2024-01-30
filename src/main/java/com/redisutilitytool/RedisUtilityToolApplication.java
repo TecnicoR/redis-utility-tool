efd0e9e73a9fed3f44e47875ca9367ac2ba49700
Rabi <rabinarayan.patra@thoughtclan.com>
2024-01-30 12:02:37 +0530

    Initial: Project setup.
    Added necessary dependencies.

---
+++ src/main/java/com/redisutilitytool/RedisUtilityToolApplication.java
@@ -0,0 +1,13 @@
+package com.redisutilitytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisUtilityToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisUtilityToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redisutilitytool/RedisUtilityToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redisutilitytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisUtilityToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
